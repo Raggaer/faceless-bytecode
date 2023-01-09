a0a3b83958c73047fa2fbc57af7f16af3246de93
Raggaer <nakotoffana@gmail.com>
2023-01-10 00:51:54 +0100

    Simple test to compare gas usage

--- hardhat/contracts/SimpleSVG.sol
+++ hardhat/contracts/SimpleSVG.sol
@@ -1,5 +1,6 @@
 pragma solidity ^0.8.4;
 
+// Contract used only for testing and comparing gas usage
 contract SimpleSVG {
   function getSvg(address _addr) public view returns (string memory) {
     bytes memory c = _addr.code;
